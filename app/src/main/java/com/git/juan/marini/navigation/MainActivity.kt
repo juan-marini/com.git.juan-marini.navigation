00a8be33b964549a67aeccbc11a94d8e31fae257
juan-marini <juanmarinidoc@gmail.com>
2026-03-16 21:34:14 -0300

    Remove exemplos e limpa MainActivity

--- app/src/main/java/com/git/juan/marini/navigation/MainActivity.kt
+++ app/src/main/java/com/git/juan/marini/navigation/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.git.juan.marini.navigation.ui.theme.NavigationbetweenscreensTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationbetweenscreensTheme {
+                @Suppress("UnusedMaterial3ScaffoldPaddingParameter")
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationbetweenscreensTheme {
-        Greeting("Android")
     }
 }
